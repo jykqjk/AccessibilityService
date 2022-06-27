f8b1ab6bb99544746b00cde56518119911bf9472
jiangyu <1733382070@qq.com>
2022-06-27 20:34:36 +0800

    New package AccessibilityMode

---
+++ app/src/main/java/com/example/myaccessibilitymode/MyAccessibilityService.java
@@ -0,0 +1,16 @@
+package com.example.myaccessibilitymode;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
